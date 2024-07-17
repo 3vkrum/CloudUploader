3e9e3069e1c153bdf7007793dd6e283dda4a4756
3vkrum <98079598+3vkrum@users.noreply.github.com>
2024-07-17 21:05:26 +0530

    Add error handling - no argument

--- cloud-uploader.sh
+++ cloud-uploader.sh
@@ -1,1 +1,10 @@
 #!/bin/bash
+# echo "These are your available buckets:"
+# aws s3 ls
+# echo "Choose a bucket to upload into:"
+# read bkt_name
+if [ $# -eq 0 ]; then
+    echo "Usage: clouduploader <filename>"
+    echo "This command requires an argument."
+    exit 1
+fi
